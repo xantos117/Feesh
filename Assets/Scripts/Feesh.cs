ad65b948629a85b113a8bd46f83d9c77db6747cb
squirrelbaffler <xantos117@gmail.com>
2021-12-14 00:09:03 -0500

    Reverted inheritance to make webassembly build work more efficiently. Apparently that's a thing.

--- Assets/Scripts/Feesh.cs
+++ Assets/Scripts/Feesh.cs
@@ -2,47 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Feesh : Mover
+public class Feesh : MonoBehaviour
 {
-    // leader values
+    Material mat;
+    Transform target;
+    Transform cachedXform;
+
+    public FeeshSettings fs;
     public float keyRadius = 5.0f;
     public float angleRads = 0.0f;
-    public Feesh leader;
-    public bool amLeader = false;
 
-    // internal values regarding neighbors, updated by the spawner
+    public Vector3 velocity;
+    public Vector3 forceTotal = Vector3.zero;
+    public bool showViewCircle = false;
+    public bool feeshOfInterest = false;
     private Vector3 neighborCenter;
     private Vector3 neighborDistance;
     private Vector3 neighborDirection;
     private Vector3 predCenter;
     private Vector3 predDirection;
+    public Feesh leader;
     private bool fleeing = false;
     private float panicking = 0.0f;
 
+    public bool amLeader = false;
+
+    private void Awake() {
+        mat = transform.GetComponentInChildren<MeshRenderer>().material;
+        cachedXform = transform;
+    }
+
     private void Update() {
+        Vector3 acceleration = Vector3.zero;
+        acceleration = acceleration - transform.position;
+        acceleration.Normalize();
+        acceleration *= fs.angVel;
         cachedXform.position += velocity * Time.deltaTime;
 
-        // feesh that leave the boundary get teleported to the inside of the opposite side
         if(cachedXform.position.sqrMagnitude > fs.wallBoundary * fs.wallBoundary) {
             float mag = cachedXform.position.magnitude;
             cachedXform.position = cachedXform.position.normalized * 0.99f * mag * -1;
             //cachedXform.position *= -1;
         }
 
-        applyForce(randWalkForce() * fs.wanderStrength);
+        Vector3 curPos = transform.position;
+        angleRads += fs.angVel * Time.deltaTime;
+        if(angleRads > 2 * Mathf.PI) {
+            angleRads -= 2 * Mathf.PI;
+        }
 
-        // move differently dependent on state
         if (amLeader) {
-            // calculate keyed circular movement
-            Vector3 acceleration = Vector3.zero;
-            acceleration = acceleration - transform.position;
-            acceleration.Normalize();
-            acceleration *= fs.angVel;
             velocity += acceleration * Time.deltaTime;
-            angleRads += fs.angVel * Time.deltaTime;
-            if(angleRads > 2 * Mathf.PI) {
-                angleRads -= 2 * Mathf.PI;
-            }
 
             cachedXform.position = new Vector3(keyRadius * Mathf.Sin(angleRads), 0, keyRadius * Mathf.Cos(angleRads));
             cachedXform.forward = Vector3.Cross(Vector3.up, cachedXform.position).normalized;
@@ -53,7 +63,7 @@ public class Feesh : Mover
             cachedXform.forward = velocity.normalized;
             transform.position = cachedXform.position;
             transform.forward = cachedXform.forward;
-            // a panicking feesh only cares about predator data, as well as the best cluster of neighbors to hide in
+
             applyForce(seekForce(predCenter) * -1 * fs.fleeWeight);
             applyForce(alignForce(predDirection) * -1 * fs.reverseWeight);
             applyForce(seekForce(neighborCenter) * fs.centerWeight);
@@ -66,17 +76,48 @@ public class Feesh : Mover
             transform.position = cachedXform.position;
             transform.forward = cachedXform.forward;
             Vector3 leaderPos = leader.transform.position;
-            // normal flocking behavior
             applyForce(seekForce(leaderPos) * fs.followTheLeader);
+
             applyForce(seekForce(neighborCenter) * fs.attraction);
+
             applyForce(seekForce(neighborDistance) * -1 * fs.avoidance);
+
             applyForce(alignForce(neighborDirection) * fs.alignment);
 
             velocity += forceTotal * Time.deltaTime;
             velocity = velocity.normalized * fs.maxSpeed;
         }
     }
-    // enable some visualization in the editor
+
+    void applyForce(Vector3 inForce) {
+        forceTotal += inForce;
+    }
+
+    private Vector3 tanForce(Vector3 predDir) {
+        // get the direction directly away from the predator's velocity
+        Vector3 orth = Vector3.Cross(velocity,predDir);
+        Vector3 desVel = Vector3.Cross(predDir,orth);
+        desVel.Normalize();
+        return desVel * fs.maxSpeed;
+    }
+
+    private Vector3 seekForce(Vector3 targetPos) {
+        Vector3 resForce = new Vector3();
+        Vector3 curPos = transform.position;
+        Vector3 desiredVel = targetPos - curPos;
+        desiredVel = desiredVel.normalized * fs.maxSpeed;
+
+        resForce = desiredVel - velocity;
+
+        return resForce;
+    }
+
+    private Vector3 alignForce(Vector3 targetDir) {
+        Vector3 resForce = targetDir.normalized - velocity.normalized;
+
+        return resForce;
+    }
+
     private void OnDrawGizmos() {
         if (showViewCircle) {
             Gizmos.color = Color.gray;
@@ -85,41 +126,34 @@ public class Feesh : Mover
         }
     }
 
-    // iterate over neighbors provided by the spawner, calculate important values
     public void checkNeighbors(List<Feesh> fList,List<Predator> pList) {
-        // some initialization
         Vector3 avgNeighborPosition = new Vector3();
         Vector3 separationForce = new Vector3();
         Vector3 avgFwd = velocity;
         int nNeighbors = 0;
-        // iterate over fellow Feesh
-        foreach(Feesh fsh in fList) {
-            if (this.GetInstanceID() != fsh.GetInstanceID()) {
-                Vector3 distanceToNeighbor = fsh.transform.position - transform.position;
+        for (int i = 0; i < fList.Count; i++) {
+            if (this.GetInstanceID() != fList[i].GetInstanceID()) {
+                Vector3 distanceToNeighbor = fList[i].transform.position - transform.position;
                 float dotToNeighbor = Vector3.Dot(distanceToNeighbor.normalized, transform.forward.normalized);
                 float angleToNeighbor = Mathf.Abs(Mathf.Acos(dotToNeighbor) * 180 / Mathf.PI);
-                // only gather information about neighbors within the view bubble
                 if (distanceToNeighbor.sqrMagnitude < fs.viewDistance * fs.viewDistance && fs.viewAngle > angleToNeighbor) {
                     nNeighbors++;
-                    avgNeighborPosition += fsh.transform.position;
-                    avgFwd += (fsh.velocity);
+                    avgNeighborPosition += fList[i].transform.position;
+                    avgFwd += (fList[i].velocity);
                     if (feeshOfInterest) {
-                        Debug.DrawLine(transform.position, fsh.transform.position, Color.green, 0.0f, false);
+                        Debug.DrawLine(transform.position, fList[i].transform.position, Color.green, 0.0f, false);
                     }
                 }
-                // separation force is the distance to the neighbor over it's squared magnitude, so long as that distance is within the avoidance radius and the neighbor is visible
                 if (distanceToNeighbor.sqrMagnitude < fs.avoidanceRadius * fs.avoidanceRadius && fs.viewAngle > angleToNeighbor) {
                     separationForce += ( distanceToNeighbor / (distanceToNeighbor.sqrMagnitude));
                 }
             }
 
         }
-        // reset some fleeing values
         fleeing = false;
         int nPredators = 0;
         Vector3 predDir = Vector3.zero;
         Vector3 avgPredPos = Vector3.zero;
-        // iterate over predators
         foreach(Predator p in pList) {
             Vector3 threatDist = p.transform.position - transform.position;
             float dotToThreat = Vector3.Dot(threatDist.normalized, transform.forward.normalized);
--- Assets/Scripts/Predator.cs
+++ Assets/Scripts/Predator.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Predator : Mover
+public class Predator : MonoBehaviour
 {
-
+    Material mat;
+    Transform target;
+    Transform cachedXform;
+    public Vector3 velocity;
+    public Vector3 forceTotal = Vector3.zero;
+    public bool showViewCircle = false;
+    public bool feeshOfInterest = false;
     private bool hunting = false;
     private Vector3 preyPos;
+
+    public FeeshSettings fs;
+    private void Awake() {
+        mat = transform.GetComponentInChildren<MeshRenderer>().material;
+        cachedXform = transform;
+    }
 
     private void Update() {
         forceTotal = Vector3.zero;
@@ -31,6 +43,39 @@ public class Predator : Mover
 
         velocity += forceTotal * Time.deltaTime;
         velocity = velocity.normalized * fs.maxSpeed;
+    }
+
+    void applyForce(Vector3 inForce) {
+        forceTotal += inForce;
+    }
+
+    private Vector3 seekForce(Vector3 targetPos) {
+        Vector3 resForce = new Vector3();
+        Vector3 curPos = transform.position;
+        Vector3 desiredVel = targetPos - curPos;
+        desiredVel = desiredVel.normalized * fs.maxSpeed;
+
+        resForce = desiredVel - velocity;
+
+        return resForce;
+    }
+
+    private Vector3 randWalkForce() {
+        //Vector3 resDir = velocity;
+        /*float randVal = Random.value;
+        if (randVal < 0.1) {
+            resDir = Quaternion.AngleAxis(fs.randWalkAngle, transform.right) * resDir;
+        } else if (randVal >= 0.1 && randVal < 0.5) {
+            resDir = Quaternion.AngleAxis(fs.randWalkAngle, transform.up) * resDir;
+        } else if (randVal >= 0.5 && randVal < 0.9) {
+            resDir = Quaternion.AngleAxis(fs.randWalkAngle, -1 * transform.up) * resDir;
+        } else {
+            resDir = Quaternion.AngleAxis(fs.randWalkAngle, -1 * transform.right) * resDir;
+        }*/
+        //resDir += Random.insideUnitSphere;
+
+
+        return Random.insideUnitSphere;
     }
 
     public void choosePrey(List<Feesh> fList) {
